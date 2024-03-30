0fbcfa4d27a7e5994050b99610ae89b085aa8a2a
s1r1luxx <155615882+s1r1luxx@users.noreply.github.com>
2024-03-30 19:06:01 +0700

    User

---
+++ App.js
@@ -0,0 +1,12 @@
+import LoginSignup from './LoginSignup/LoginSignup';
+import User from './pages/User';
+
+function App() {
+  return (
+    <div>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
